02047113357f04f7016797a49c71cf744e4e78c9
Franco Delocca <52047668+FDelocca2305@users.noreply.github.com>
2024-09-24 15:30:36 -0300

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const path = require("path");
 const TelegramBot = require("node-telegram-bot-api");
 
 const gameName = "Pocunitytelegram";
-const webURL = "gusty-checkered-knee.glitch.me";
+const webURL = "poc-unity.glitch.me";
 
 const server = express();
 const bot = new TelegramBot(process.env.BOT_TOKEN, { polling: true });
